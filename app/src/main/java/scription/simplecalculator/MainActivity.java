0c7d3e063df3eb4171226ef2a6846fd433dc78d4
Walter Hanson <wfh661@gmail.com>
2019-01-04 20:05:31 -0800

    Basic functionality without decimal numbers.

--- app/src/main/java/scription/simplecalculator/MainActivity.java
+++ app/src/main/java/scription/simplecalculator/MainActivity.java
@@ -8,38 +8,110 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private float total = 0f;
-    private char operand = ' ';
-    private TextView displayBox = (TextView) findViewById(R.id.resultBox);
+    private double total;
+    private String operand;
+    private TextView displayBox;
+    private boolean positive;
+    private String currentValue;
+    private boolean firstValue;
+    private boolean end;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        total = 0;
+        operand = "";
+        positive = true;
+        displayBox = findViewById(R.id.resultBox);
+        currentValue = "";
+        firstValue = true;
+        end = false;
     }
 
     public void addValue(View view) {
+        if(end) {
+            end = false;
+            firstValue = true;
+            total = 0;
+        }
         Button b = (Button) view;
         String buttonText = b.getText().toString();
-        int value = Integer.valueOf(buttonText);
-        switch(operand) {
-            case '+':
-                total += value;
-            case '-':
-                total -= value;
-            case '*':
-                total *= value;
-            case '/':
-                total /= value;
-            default:
-                total = value;
-        }
-        displayBox.setText(Float.toString(total));
+        currentValue += buttonText;
+        displayBox.setText(currentValue);
     }
 
     public void setOperand(View view) {
         Button b = (Button) view;
-        operand = b.getText().charAt(0);
+        operand = b.getText().toString();
+        if(end) {
+            end = false;
+        }
+        else if(!currentValue.equals("") && !firstValue) {
+            switch (operand) {
+                case "+":
+                    total += Double.valueOf(currentValue);
+                    break;
+                case "-":
+                    total -= Double.valueOf(currentValue);
+                    break;
+                case "*":
+                    total *= Double.valueOf(currentValue);
+                    break;
+                case "/":
+                    total /= Double.valueOf(currentValue);
+                    break;
+            }
+        } else {
+            total += Double.valueOf(currentValue);
+        }
+        currentValue = "";
         displayBox.setText(operand);
+    }
+
+    public void displayResult(View view) {
+        switch (operand) {
+            case "+":
+                total += Double.valueOf(currentValue);
+                break;
+            case "-":
+                total -= Double.valueOf(currentValue);
+                break;
+            case "*":
+                total *= Double.valueOf(currentValue);
+                break;
+            case "/":
+                total /= Double.valueOf(currentValue);
+                break;
+        }
+        displayBox.setText(Double.toString(total));
+        currentValue = "";
+        operand = "";
+        positive = true;
+        end = true;
+    }
+
+    public void changeSign(View view) {
+        if(positive) {
+            positive = false;
+            currentValue = "-" + currentValue;
+            displayBox.setText(currentValue);
+        } else {
+            positive = true;
+            if(currentValue.charAt(0) == '-') {
+                currentValue = currentValue.substring(1);
+                displayBox.setText(currentValue);
+            }
+        }
+    }
+
+    public void clearPressed(View view) {
+        total = 0;
+        operand = "";
+        positive = true;
+        currentValue = "";
+        displayBox.setText("0");
+        firstValue = true;
+        end = false;
     }
 }
